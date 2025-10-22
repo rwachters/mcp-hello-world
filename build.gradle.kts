011041063d68d8879cf5036cd302038fdd368e73
Rogier Wachters <r@wachters.eu>
2025-10-22 14:43:31 +0200

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.2.20"
+}
+
+group = "eu.torvian"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
